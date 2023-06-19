a8ccc8d5caee74d54c58081fd7b6841bfed29f25
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2023-06-19 12:24:04 +0300

    test1

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public double MinAVG(string[] marks)
+        {
+            if (marks == null || marks.Length == 0)
+            {
+                throw new ArgumentException("The 'marks' array cannot be null or empty");
+            }
+           
+
+
+
+            int[] mark = Array.ConvertAll(marks, s => int.Parse(s));
+
+            int sum = mark[0] + mark[1] + mark[2] + mark[3] + mark[4];
+
+
+            double avg = (double)sum / marks.Length;
+            double roundedavg = Math.Floor(avg);
+            return roundedavg;
+        }
+    }
+    }
+
---
+++ ClassLibrary2Tests/Class1Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary2.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void MinAVGTest()
+        {
+            string[] marks = { "4", "5", "3", "2", "4" };
+            double expected = 3.0;
+            
+            Class1 calculator = new Class1();
+            double result = calculator.MinAVG(marks);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void MinAVGTest1()
+        {
+            string[] marks = { };
+            double expected = 3.0;
+
+            Class1 calculator = new Class1();
+            double result = calculator.MinAVG(marks);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void MinAVGTest2()
+        {
+            string[] marks = { "5", "4", "3", "2", "4" };
+            double expected = 3.0;
+
+            Class1 calculator = new Class1();
+            double result = calculator.MinAVG(marks);
+            Assert.AreEqual(expected, result);
+        }
+    }
+ 
+
+
+}
